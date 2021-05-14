3578581c77504a28db47be4ea2e8e6177d9cddbd
StepanDrogin <drogenko.stepan@yandex.ru>
2021-05-14 15:07:09 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React, {useContext} from 'react';
+import {BrowserRouter} from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import AppRouter from "./components/AppRouter";
+import './App.css'
+import {Context} from "./index";
+import {useAuthState} from "react-firebase-hooks/auth";
+import Loader from "./components/Loader";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
-}
+const App = () => {
+    const {auth} = useContext(Context)
+    const [user, loading, error] = useAuthState(auth)
+
+    if (loading) {
+        return <Loader/>
+    }
+
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <AppRouter/>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import firebase from "firebase";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBVWjtzmLenPfVW0WMi6l2bjfi2eM0nzxc",
+    authDomain: "chat-fddc5.firebaseapp.com",
+    projectId: "chat-fddc5",
+    storageBucket: "chat-fddc5.appspot.com",
+    messagingSenderId: "488830618123",
+    appId: "1:488830618123:web:6aa8c8d0480a59f4ed550e"
+    }
+);
+
+export const Context = createContext(null)
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Context.Provider value={{
+        firebase,
+        auth,
+        firestore
+    }}>
+        <App />
+    </Context.Provider>,
   document.getElementById('root')
 );
+
